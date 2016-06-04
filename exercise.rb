10de81a8e94d2190a7c755c58f9f8411d888bc4e
Ayuta Omiya <ayuta.osx@gmail.com>
2016-06-04 11:20:49 +0000

    4.6 演習3

--- exercise.rb
+++ exercise.rb
@@ -1,7 +1,17 @@
 class Exercise
-    def string_shuffle(s)
-      s.split('').shuffle.join
-    end
+
+  def string_shuffle(s)
+    s.split('').shuffle.join
+  end
+  
+  def hash_exercise()
+    person1 = {first:"Ayuta", last:"Omiya"}
+    person2 = {first:"Taro", last:"Test"}
+    person3 = {first:"Jiro", last:"Excerise"}
+    params = {:father => person1, :mother => person2, :child => person3}
+    puts(params[:father][:first])
+  end
+
 end
 
 class String
