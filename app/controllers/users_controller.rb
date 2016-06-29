65448e16130153397605a0bcea84ac8bb2b2504e
Ayuta Omiya <ayuta.osx@gmail.com>
2016-06-29 07:23:32 +0000

    7.3.2 Strong parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])    # 実装は終わっていないことに注意!
+    @user = User.new(user_params)
     if @user.save
       # Handle a successful save.
     else
@@ -16,4 +16,9 @@ class UsersController < ApplicationController
     end
   end
 
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
