f82c3833dfd57e162a750a0982525b9f9f7bf147
Ayuta Omiya <ayuta.osx@gmail.com>
2016-06-29 07:20:20 +0000

    7.3.1 正しいフォーム

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(params[:user])    # 実装は終わっていないことに注意!
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
 end
