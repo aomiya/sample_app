10c0bb202eb3b63f085070060fa1cc60ad8d6650
Ayuta Omiya <ayuta.osx@gmail.com>
2016-07-05 02:18:05 +0000

    8.4.3 ユーザーを忘れる

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,9 @@ class User < ActiveRecord::Base
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
+
+  # ユーザーログインを破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
