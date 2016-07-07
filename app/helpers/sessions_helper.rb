29aa8daaa270715b2aaf0664a2beebc5f8ea6ee9
Ayuta Omiya <ayuta.osx@gmail.com>
2016-07-07 03:45:01 +0000

    9.2.2 正しいユーザーを要求する

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,6 +9,12 @@ module SessionsHelper
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
+
+  # 与えられたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+
   # 記憶トークンcookieに対応するユーザーを返す
   def current_user
     if (user_id = session[:user_id])
